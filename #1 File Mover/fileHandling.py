7925c98d5f0f880fedce4d500f1e565ef85c1dce
Theo <prstylukman@gmail.com>
2025-12-22 23:41:27 +0700

    add feature for custom file's format and directory

--- #1 File Mover/fileHandling.py
+++ #1 File Mover/fileHandling.py
@@ -1,20 +1,42 @@
+#!/usr/bin/env python3
 import shutil
 import os
 import re
 
-filePath = '/home/theo/code/learn-python/'
+filePath = os.getcwd()
+
+print('Current path:', filePath)
 
 files = [x for x in os.listdir(filePath) if os.path.isfile(os.path.join(filePath, x))]
-print(files)
 
-r = re.compile('.*.pdf')
-fileName = list(filter(r.match, files))
+while True:
+    format = str(input('Enter file\'s format: '))
 
-for x in fileName:
-    shutil.move(x, f'{filePath}pdfFile/{x}')
+    r = re.compile(f'.*.{format}')
+    fileName = list(filter(r.match, files))
 
-r = re.compile('.*.txt')
-fileName = list(filter(r.match, files))
+    if len(fileName) == 0:
+        print('There is no matched file\'s format')
+    else:
+        break
 
-for x in fileName:
-    shutil.move(x, f'{filePath}txtFile/{x}')
+targetPath = str(input('Target directory: '))
+
+try:
+    os.mkdir(targetPath)
+    print(f'Directory {targetPath} created successfully.')
+except PermissionError:
+    print(f'Permission denied: Unable to create {targetPath}')
+    exit()
+except Exception as e:
+    print(f'An error occurred: {e}')
+    exit()
+
+try:
+    for x in fileName:
+        shutil.move(x, f'{filePath}/{targetPath}/{x}')
+
+    print('Your files is now organized.')
+except Exception as e:
+    print(f'An error occurred: {e}')
+    
